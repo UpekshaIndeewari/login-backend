fcc181fb21b6f8161a9c7c4ee248c416d80d5f9b
Upeksha Indeewari <indiedirisooriya@gmail.com>
2026-01-05 22:08:49 +0100

    Update database.py

--- app/db/database.py
+++ app/db/database.py
@@ -1,14 +1,3 @@
-# db/database.py
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker, declarative_base
-
-# DATABASE_URL = "postgresql://postgres:root@localhost:5432/userDB"
-
-# engine = create_engine(DATABASE_URL)
-# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Base = declarative_base()
-
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
